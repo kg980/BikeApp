fce50de5a65ec969a238969badaf2a3846700929
Kinga <k.garbowska1@gmail.com>
2023-02-13 22:09:59 +0000

    trying to get accordion list to work

--- BikeAppExpo/src/components/PartsCard/PartsCard.js
+++ BikeAppExpo/src/components/PartsCard/PartsCard.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Pressable, Image} from 'react-native';
+import React, { useState, useTransition } from "react";
+import { View, Text, StyleSheet, Pressable, Image, Easing, Animated} from 'react-native';
 
 const PartsCard = ({title}) => {
     const [open, setOpen] = useState(false);
+    const transition = useTransition(open, open ? 0 : 1, open ? 1 : 0, 400, Easing.inOut(Easing.ease))
+    //const height = Interpolate(transition, 0, 8);
+    //const bottomRadius = Interpolate(transition, 8, 0);
 
     return (
-        <View style={styles.root}>
+        <Animated.View style={styles.root}>
             <Text>Part Name</Text>
             <Text>Click to expand</Text>
-        </View>
+        </Animated.View>
     )
 }
 
