77be1d28c7d42b6baa52c48c74ba3438082c9fa1
Kinga <k.garbowska1@gmail.com>
2023-02-13 21:13:03 +0000

    complete UI homepage, no functional implementation

--- BikeAppExpo/src/screens/HomeScreen/HomeScreen.js
+++ BikeAppExpo/src/screens/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React , {useState} from "react";
-import { View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
+import { View, Text, Image, StyleSheet, useWindowDimensions, Pressable} from 'react-native';
 import Bicycle from "../../../assets/images/Bicycle.png";
 import BicycleRed from "../../../assets/images/BicycleRed.png";
 import CustomInput from "../../components/CustomInput";
@@ -11,23 +11,29 @@ import CustomFooter from "../../components/CustomFooter";
 
 const HomeScreen = () => {
     const myBikesPressed = () => {
-        //authenticate
+        //
         console.warn("Load My Bikes Page");
     };
 
     const forumPressed = () => {
-        //authenticate
+        //
         console.warn("Load Forum Page");
     };
 
     const maintListPressed = () => {
-        //authenticate
+        //
         console.warn("Load maintenance Checklist Page");
     };
 
     const journeysPressed = () => {
-        //authenticate
+        //
         console.warn("Load Journey History Page");
+    };
+    const pumpIt = () => {
+        //Reset distance in database / on screen
+        //reset logo to normal
+        //add record to maintenance history
+        console.warn("Reset Distance");
     };
     
 
@@ -37,9 +43,9 @@ const HomeScreen = () => {
             <View style={styles.content}>
                 <View style={styles.distance}>
                     <Image source={Bicycle} resizeMode="contain"/>
-                    <Text>METER</Text>
-                    <Text>Distance</Text>
-                    <Text>Button</Text>
+                    <Text style={styles.distanceText}>/4000km</Text>
+                    <Text style={styles.meter}></Text>
+                    <Pressable onPress={pumpIt} style={styles.pumpIt}><Text style={styles.pumpItText}>PumpIt!</Text></Pressable>
                 </View>
                 <View style={styles.buttonsBox}>
                     <View style={styles.buttonsCol}>
@@ -123,6 +129,38 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         height: '45%',
     },
+    pumpIt: {
+        backgroundColor: '#FF8001',
+        borderColor: '#FF8001',
+        padding: 5,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderRadius: 15,
+        fontSize: 20,
+        marginBottom: '1%',
+    },
+    pumpItText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    distanceText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    distanceTextRed: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'red',
+    },
+    meter: {
+        backgroundColor: 'white',
+        width: '80%',
+        height: '15%',
+        borderWidth: 1,
+        borderRadius: 15,
+        borderColor: 'white',
+        },
 });
 
 export default HomeScreen;
