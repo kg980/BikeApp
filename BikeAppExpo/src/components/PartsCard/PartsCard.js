9df214fd080d18033325f05aef7ce4819f5a7162
Kinga <k.garbowska1@gmail.com>
2023-02-14 19:17:12 +0000

    decided not to use the Animated from react native for the accordion menu

--- BikeAppExpo/src/components/PartsCard/PartsCard.js
+++ BikeAppExpo/src/components/PartsCard/PartsCard.js
@@ -1,17 +1,14 @@
 import React, { useState, useTransition } from "react";
-import { View, Text, StyleSheet, Pressable, Image, Easing, Animated} from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image} from 'react-native';
 
 const PartsCard = ({title}) => {
-    const [open, setOpen] = useState(false);
-    const transition = useTransition(open, open ? 0 : 1, open ? 1 : 0, 400, Easing.inOut(Easing.ease))
-    //const height = Interpolate(transition, 0, 8);
-    //const bottomRadius = Interpolate(transition, 8, 0);
+    
 
     return (
-        <Animated.View style={styles.root}>
+        <View style={styles.root}>
             <Text>Part Name</Text>
             <Text>Click to expand</Text>
-        </Animated.View>
+        </View>
     )
 }
 
